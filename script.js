7262ff1d8b75e3d0f662ac0517f8037bfb9a7c30
jannik <j.pohlmann@outlook.de>
2025-02-26 23:51:02 +0100

    added logging , also some other changes that i didnt commit, oops also changed the style

--- script.js
+++ script.js
@@ -1,63 +1,105 @@
 
 
 let sendformHTML =document.getElementById("sendform");
+let consoleOutputEle =document.getElementById("consoleOutputEle");
 let category = document.getElementById("wcategory").value;
 let newcategory = document.getElementById("wncategory").value;
 let level = document.getElementById("wlevel").value;
 let newlevel = document.getElementById("wnlevel").value;
 let apikey = document.getElementById("apikey").value;
 let haslevel = false;
+let runcount = 1;
+
+
+function log(text){
+    consoleOutputEle.value+=text+'\n';
+    consoleOutputEle.scrollTop=consoleOutputEle.scrollHeight;
+}
+
 sendformHTML.addEventListener("submit",(e)=>{
     e.preventDefault();
+
+
+    if(category.value==""||newcategory.value==""){
+        log("missing input values...");
+        return;
+    }
+
+    log("");
+    log("starting to move runs!");
+
     let Url;
     if(level&&newlevel){
-         Url=`https://www.speedrun.com/api/v1/runs?category=${category}&max=10&level=${level}`;
-         haslevel = true;
+        Url=`https://www.speedrun.com/api/v1/runs?category=${category}&level=${level}&max=${runcount}&status=verified`;
+        haslevel = true;
+        log("moving runs with levels...");
     }else{
-
-        Url =`https://www.speedrun.com/api/v1/runs?category=${category}&max=10 `;
+        Url =`https://www.speedrun.com/api/v1/runs?category=${category}&max=${runcount}&status=verified `;
     }
     
     fetch(Url)
     .then(data=>{return data.json()})
-    .then(res=>{changecatjson(res)})
-    .catch(err=>{console.log(err)})
+    .then(res=>{
+        log("recieved old runs...");
+        changecatjson(res);
+    })
+    .catch(error=>{
+        console.log(error);
+        log(error);
+    })
 
 })
 
 function changecatjson(catjson){
-    catjson.data.forEach(run => {
 
-        console.log(run)
+    let i = 0;
+    catjson.data.forEach(run => {
+        log("transforming run: "+i);
+        
+        run.category = newcategory;
+        
+        if(haslevel){
+            run.level = newlevel;
+        }else{
+            delete run.level;
+        }
+        if(!run.data){
+            delete run.data;
+        }
+        if(run.region){
+            run.region = run.system.region;
+        }
+        if(run.system.platform){
+            run.platform = run.system.platform;
+        }
+
+        run.verified=true;
+        
+        run.times.realtime =run.times.realtime_t
+        run.times.ingame =run.times.ingame_t
+        run.times.realtime_noloads =run.times.realtime_noloads_t;
+
+        run.players.forEach(p=>{
+            delete p.uri;
+        })
+        run.emulated = run.system.emulated;
+        
+        if(run.videos){
+            run.video = run.videos.links[0].uri;
+        }
+        if(!run.comment){
+            delete run.comment;
+        }
+        if(run.splits){
+            run.splitsio = run.splits.uri;
+        }
         for(var name in run.values) {
             run.variables[name].value = run.values[name];
             run.variables[name].type = "pre-defined";
         }
 
-        if(haslevel){
-            run.level = newlevel;
-        }else{
-            run.level = null
-        }
-        if(run.splits){
-            run.splitsio = run.splits.uri;
-        }else{
-            run.splitsio = null
-        }
-        run.category = newcategory;
-        if(run.videos){
-            run.video = run.videos.links[0].uri;
-        }
-        run.times.realtime =run.times.realtime_t
-        run.times.ingame =run.times.ingame_t
-        run.times.realtime_noloads =run.times.realtime_noloads_t;
-        run.verified=true;
-        run.platform = run.system.platform;
-        run.emulated = run.system.emulated;
-        run.region = run.system.region;
-        run.variables = {}
-        run.variables[run.values.k]
         delete run.values;
+        delete run.submitted;
         delete run.id;
         delete run.links;
         delete run.videos;
@@ -70,25 +112,37 @@ function changecatjson(catjson){
         delete run.times.realtime_t;
         delete run.times.realtime_noloads_t;
         delete run.game;
-        run.players.forEach(p=>{
-            delete p.uri;
-        })
-    
-
+        
+        i++;
     });
-    console.log(catjson.data)
 
-    postnewcategories(catjson.data);
+    log("transformed all runs...");
+
+    
+    i = 0;
+    let postint = setInterval(()=>{
+        console.log("posting request "+ i);
+        log("posting new run request with index"+ i);
+        postnewcategories(catjson.data[i]);
+        if(i>=catjson.pagination.size-1){
+            console.log("reached end at "+ i);
+            log("reached end at run index:"+ i);
+            clearInterval(postint);
+        }
+        i++;
+    },Math.round((catjson.pagination.size*1000)/50))
 
 }
+
+
 
 function postnewcategories(data){
 
     const Url = "https://www.speedrun.com/api/v1/runs";
-    const Data = JSON.stringify({run:data[0]});
+    const Data = JSON.stringify({run:data});
     const para = {
         headers:{
-            "Content-Type":"application/json; charset=UTF-8",
+            "Content-Type":"application/json",
             "X-API-Key":apikey,
         },
         body:Data,
@@ -96,9 +150,15 @@ function postnewcategories(data){
     };
 
     fetch(Url,para)
-    .then(data=>{return data.json()})
-    .then(res=>{console.log(res)})
-    .catch(error=>{console.log(error)});
+    .then(res=>{
+        log("response: "+res.status);
+        console.log(res)
+    })
+    .catch(error=>{
+        console.log(error);
+        log(error);
+    });
 
 
 }
+
